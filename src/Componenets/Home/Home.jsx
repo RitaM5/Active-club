6835f8368aaba7381f18c033ba807cd18fc4cc89
RitaM5 <sporshia108514orva@gmail.com>
2022-12-08 20:18:14 +0600

    card created in services section

--- src/Componenets/Home/Home.jsx
+++ src/Componenets/Home/Home.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import Service from '../Service/Service';
+
 import './Home.css'
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('Data.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
+    return (
+        <>
+            <div className='row'>
+                <div className="col-lg-8">
+                    <div className='heading'>
+                        <h3><img className='me-2' src="https://img.icons8.com/external-flaticons-flat-flat-icons/64/null/external-exercise-working-stress-flaticons-flat-flat-icons-2.png" />UTRA-ACTIVE-CLUB</h3>
+                        <p className='fs-5'>Select today’s exercise</p>
+                    </div>
+                    <div className="row py-2">
+                        {
+                            services.map(items =>
+                                <Service
+                                    key={items.id}
+                                    items={items} />)
+                        }
+                    </div>
+                </div>
+                <div className="col-lg-4">
+
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Home
---
+++ src/Componenets/Service/Service.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import './Service.css'
+const Service = (props) => {
+    const { name, picture, details, age, time } = props.items;
+    return (
+        <div className='col-lg-4 col-md-4 col-12'>
+            <div className="bg-white rounded Larger shadow">
+                <img className="card-img p-2" src={picture} alt="" style={{ height: 150, borderRadius: 20 }} />
+                <div className='card-part ps-2'>
+                    <h4>{name}</h4>
+                    <p>{details}</p>
+                    <p className='fw-bold'>For Age : {age}</p>
+                    <p className='fw-bold'>Time required : {time}</p>
+                   <p className='pb-2'><Button variant="info">Add to List</Button>{' '}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Service
